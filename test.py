c6d172bdc9b94c8cf25f8c39d1c5da742b08d72a
EC2 Default User <ec2-user@ip-10-0-0-152.ec2.internal>
2022-07-03 18:57:32 +0000

    updated version

--- test.py
+++ test.py
@@ -1,8 +1,44 @@
 #!/usr/bin/python3
 
-def helloWorld():
-    print("Hello World")
+'''
+    Sum of two numbers
+'''
+def sumOfTwoNumbers():
+    a = int(input('Enter first value:'))
+    b = int(input('Enter second value:'))
+    sumVal = a + b
+    print('Sum Of two numbers:{}'.format(sumVal))
 
+
+def exampleDictionary():
+    dictionary = {}
+
+    # print type of dictonary
+    print('Type of dictonary {}'.format(type(dictionary)))
+
+    dictionary["key1"] = "value1"
+    dictionary[42] = "This is a test"
+    dictionary[1.2] = "testing is an experiment"
+    print(dictionary)
+
+    # print all keys in dictionary
+    print("-------------------------------")
+    print("Keys:-->")
+    for k in dictionary.keys():
+        print('\b {}'.format(k)) 
+
+    # print all keys in dictionary
+    print("-------------------------------")
+    print("Values:-->")
+    for v in dictionary.values():
+        print('\b {}'.format(v)) 
+
+    print("-------------------------------")
+    print("Key Value:-->")
+    for k, v in dictionary.items():
+        print('\b {} {}'.format(k, v)) 
 
 if __name__ == '__main__':
-    helloWorld()
+    #sumOfTwoNumbers()
+    exampleDictionary()
+
